71862610cfdac4b049768ec66f83bb444859499a
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-05-12 13:09:52 +0200

    Added more conditions and tests

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator.Tests
+namespace StringCalculator.Tests
 {
-    internal class CalculatorTests
+    public class CalculatorTests
     {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void Add_AddsUpToTwoNumbers_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("10,90,10,20", 130)]
+        public void Add_AddsUpToAnyNumber_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("10\n90,10\n20", 130)]
+        public void Add_AddsNumberUsingNewLineDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//;\n1;2;4", 7)]
+        public void Add_AddsNumberUsingCustomDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            var result = sut.Add(calculation);
+
+            //Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1,2,-1", "-1")]
+        [InlineData("//;\n1;-2;-4", "-2,-4")]
+        public void Add_ShouldThrowAnException_WhenNegativeNumbersAreUsed(string calculation, string negativeNumbers)
+        {
+            //Arrange
+            var sut = new Calculator();
+
+            //Act
+            Action action = () => sut.Add(calculation);
+
+            //Assert
+            action.Should().Throw<Exception>().WithMessage("Negatives not allowed: " + negativeNumbers);
+        }
     }
 }
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            var delimiters = new List<char> { ',', '\n'};
+
+            if(numbers.StartsWith("//"))
+            {
+                var splitOnFirstNewLine = numbers.Split(new[] { '\n'}, 2);
+                var customDelimeter = splitOnFirstNewLine[0].Replace("//", string.Empty).Single();
+
+                delimiters.Add(customDelimeter);
+                numbers = splitOnFirstNewLine[1];
+            }
+
+            var splitNumbers = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var negativeNumbers = splitNumbers.Where(x => x < 0).ToList();
+
+            if(negativeNumbers.Any())
+            {
+                throw new Exception("Negatives not allowed: " + String.Join(",", negativeNumbers));
+            }
+
+            return splitNumbers.Sum();
+        }
+    }
+}
